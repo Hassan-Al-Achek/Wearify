d0393ec0770b21f6b75a1d3994ee878b11685bc5
Hassan AL ACHEK <hassan.alacheck@gmail.com>
2023-03-30 23:58:23 +0200

    Leader Board Done

--- lib/leaderboard_screen.dart
+++ lib/leaderboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -12,14 +13,24 @@ class _LeaderboardScreenState extends State<LeaderboardScreen> {
   // Fetch top 100 users based on XP
   Future<QuerySnapshot> _fetchTopUsers() {
     final usersRef = FirebaseFirestore.instance.collection('leaderboard');
-    return usersRef.orderBy('xp', descending: true).limit(100).get();
+    return usersRef
+        .orderBy('xp', descending: true)
+        .limit(100)
+        .get()
+        .then((querySnapshot) {
+      if (kDebugMode) {
+        print('Fetched users: ${querySnapshot.docs}');
+      }
+      return querySnapshot;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Our Top Donators'),
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: Container(),
       ),
       body: FutureBuilder<QuerySnapshot>(
         future: _fetchTopUsers(),
@@ -34,21 +45,41 @@ class _LeaderboardScreenState extends State<LeaderboardScreen> {
 
           final topUsers = snapshot.data!.docs;
 
-          return ListView.builder(
-            itemCount: topUsers.length,
-            itemBuilder: (context, index) {
-              final user = topUsers[index].data() as Map<String, dynamic>;
-              final username = user['username'];
-              final avatarUrl = user['avatar_url'];
-              final xp = user['xp'];
+          return ListView(
+            children: [
+              const SizedBox(height: 16),
+              Center(
+                child: Text(
+                  'Our Top Donators',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: topUsers.length,
+                itemBuilder: (context, index) {
+                  final user = topUsers[index].data() as Map<String, dynamic>;
+                  final username = user['username'];
+                  if (kDebugMode) {
+                    print('User data: $user');
+                  }
+                  final avatarUrl = user['avatar_url'];
+                  final xp = user['xp'];
 
-              // Display top 3 users differently
-              if (index < 3) {
-                return _buildTopThreeUser(index, username, avatarUrl, xp);
-              } else {
-                return _buildUserRow(username, avatarUrl, xp);
-              }
-            },
+                  // Display top 3 users differently
+                  if (index < 3) {
+                    return _buildTopThreeUser(index, username, avatarUrl, xp);
+                  } else {
+                    return _buildUserRow(username, avatarUrl, xp);
+                  }
+                },
+              ),
+            ],
           );
         },
       ),
@@ -57,10 +88,13 @@ class _LeaderboardScreenState extends State<LeaderboardScreen> {
 
   Widget _buildTopThreeUser(
       int index, String username, String avatarUrl, int xp) {
-    // Customize the appearance of the top 3 users
-    return Container(
-        // Your custom widget for the top 3 users
-        );
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(avatarUrl),
+      ),
+      title: Text('Top ${index + 1}: $username'),
+      trailing: _xpWidget(xp),
+    );
   }
 
   Widget _buildUserRow(String username, String avatarUrl, int xp) {
