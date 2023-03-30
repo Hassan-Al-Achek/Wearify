8c935fa8c7f4121eae703a502c9c14dce019bd41
Hassan AL ACHEK <hassan.alacheck@gmail.com>
2023-03-30 02:09:01 +0200

    Creating The Chat Screen

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({
+    super.key,
+    required currentUserId,
+    required chatUserId,
+  });
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
