fe16577d6accb22078100a061c4b10b6c52a668e
Hassan AL ACHEK <hassan.alacheck@gmail.com>
2023-03-31 14:25:54 +0200

    Splash Screen Breathing Effect Update

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,19 +2,41 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _opacityController;
+  late AnimationController _sizeController;
+
   @override
   void initState() {
     super.initState();
+
+    _opacityController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat(reverse: true);
+
+    _sizeController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..repeat(reverse: true);
+
     Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacementNamed(context, '/auth_gate');
     });
+  }
+
+  @override
+  void dispose() {
+    _opacityController.dispose();
+    _sizeController.dispose();
+    super.dispose();
   }
 
   @override
@@ -24,13 +46,29 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text(
-              'Welcome to Wearify',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          children: [
+            AnimatedBuilder(
+              animation: _opacityController,
+              builder: (context, child) {
+                return AnimatedBuilder(
+                  animation: _sizeController,
+                  builder: (context, child) {
+                    return Transform.scale(
+                      scale: 1.0 + (_sizeController.value * 0.05),
+                      child: Opacity(
+                        opacity: 0.5 + (_opacityController.value * 0.5),
+                        child: child,
+                      ),
+                    );
+                  },
+                  child: child,
+                );
+              },
+              child: const Text(
+                'Welcome to Wearify',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
             ),
-            SizedBox(height: 10),
-            CircularProgressIndicator(),
           ],
         ),
       ),
