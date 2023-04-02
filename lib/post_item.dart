b35976817d913b146837e79f01513913d89cb629
Hassan AL ACHEK <hassan.alacheck@gmail.com>
2023-04-03 01:19:59 +0200

    All posts posted by a user will appear on the publice profile screen now

---
+++ lib/post_item.dart
@@ -0,0 +1,266 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:wearify/public_profile_sceen.dart';
+
+class PostItem extends StatefulWidget {
+  final Map<String, dynamic> data;
+
+  const PostItem({Key? key, required this.data}) : super(key: key);
+
+  @override
+  State<PostItem> createState() => _PostItemState();
+}
+
+class _PostItemState extends State<PostItem> {
+  @override
+  Widget build(BuildContext context) {
+    String? description = widget.data['description'] ?? '';
+    List<dynamic>? images = widget.data['images'] ?? [];
+    int quality = widget.data['quality'] ?? 0;
+    String postType = '';
+    IconData priceIcon = Icons.attach_money;
+    String price = '';
+    int clothesType = widget.data['clothesType'] ?? 0;
+    String customClothesType = widget.data['customClothesType'] ?? '';
+    String size = widget.data['size'] ?? 'Others';
+    String customSize = widget.data['customSize'] ?? '';
+
+    switch (widget.data['postType']) {
+      case 0:
+        postType = 'For sale';
+        price = widget.data['price'] ?? '';
+        break;
+      case 1:
+        postType = 'For rent';
+        price = widget.data['price'] ?? '';
+        price += '/day';
+        break;
+      case 2:
+        postType = 'Donation';
+        price = 'Thank';
+        break;
+      default:
+        postType = 'Unknown';
+    }
+
+    String qualityIndicator = '';
+
+    switch (quality) {
+      case 0:
+        qualityIndicator = 'Excellent';
+        break;
+      case 1:
+        qualityIndicator = 'Good';
+        break;
+      case 2:
+        qualityIndicator = 'Fair';
+        break;
+      default:
+        qualityIndicator = 'Unknown';
+    }
+
+    String clothesTypeStr = '';
+
+    if (clothesType == 3) {
+      clothesTypeStr = customClothesType;
+    } else {
+      switch (clothesType) {
+        case 0:
+          clothesTypeStr = 'Shoes';
+          break;
+        case 1:
+          clothesTypeStr = 'T-shirt';
+          break;
+        case 2:
+          clothesTypeStr = 'Pants';
+          break;
+        default:
+          clothesTypeStr = 'Unknown';
+      }
+    }
+
+    String sizeStr = '';
+
+    if (size == 'Others') {
+      sizeStr = customSize;
+    } else {
+      sizeStr = size;
+    }
+
+    Widget singleSkeletonLoader() {
+      return Shimmer.fromColors(
+        baseColor: Colors.grey[300]!,
+        highlightColor: Colors.grey[100]!,
+        child: Card(
+          child: ListTile(
+            leading: const CircleAvatar(backgroundColor: Colors.grey),
+            title: Container(
+              width: double.infinity,
+              height: 10.0,
+              color: Colors.grey,
+            ),
+            subtitle: Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              height: 10.0,
+              color: Colors.grey,
+              margin: const EdgeInsets.only(top: 5.0),
+            ),
+          ),
+        ),
+      );
+    }
+
+    return FutureBuilder<DocumentSnapshot>(
+      future: FirebaseFirestore.instance
+          .collection('clients')
+          .doc(widget.data['userId'])
+          .get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return singleSkeletonLoader();
+        }
+
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
+
+        Map<String, dynamic> userData =
+            snapshot.data!.data() as Map<String, dynamic>;
+        String username = userData['username'] ?? 'Unknown';
+
+        return Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListTile(
+                leading: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PublicProfileScreen(
+                          userId: widget.data['userId'],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Semantics(
+                    label: '$username\'s avatar',
+                    child: CircleAvatar(
+                      backgroundImage: getAvatarImage(userData),
+                    ),
+                  ),
+                ),
+                title: Text(username),
+              ),
+              SizedBox(
+                height: 300,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: images?.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                      onTap: () => openImageGallery(context, images, index),
+                      child: CachedNetworkImage(
+                        imageUrl: images![index],
+                        width: MediaQuery.of(context).size.width,
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Description: $description!',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Quality: $qualityIndicator',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      postType,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    Row(
+                      children: [
+                        Icon(priceIcon),
+                        Text(
+                          price,
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Size: $sizeStr',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'Type: $clothesTypeStr',
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  ImageProvider<Object>? getAvatarImage(Map<String, dynamic> userData) {
+    if (userData['avatar_url'] != null && userData['avatar_url'] is String) {
+      return CachedNetworkImageProvider(userData['avatar_url'] as String);
+    } else {
+      return null;
+    }
+  }
+
+  void openImageGallery(
+      BuildContext context, List<dynamic> images, int initialIndex) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(),
+          body: PhotoViewGallery.builder(
+            itemCount: images.length,
+            builder: (BuildContext context, int index) {
+              return PhotoViewGalleryPageOptions(
+                imageProvider: CachedNetworkImageProvider(images[index]),
+                initialScale: PhotoViewComputedScale.contained,
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.covered * 2,
+              );
+            },
+            backgroundDecoration: const BoxDecoration(color: Colors.black),
+            pageController: PageController(initialPage: initialIndex),
+            scrollPhysics: const BouncingScrollPhysics(),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/public_profile_sceen.dart
+++ lib/public_profile_sceen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:wearify/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:wearify/post_item.dart';
 
 class PublicProfileScreen extends StatelessWidget {
   final String userId;
@@ -20,86 +21,119 @@ class PublicProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     CollectionReference clientsCollection =
         FirebaseFirestore.instance.collection('clients');
+    CollectionReference postsCollection =
+        FirebaseFirestore.instance.collection('clothes');
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: clientsCollection.doc(userId).snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return const Text('Something went wrong');
-          }
+      body: SafeArea(
+        child: StreamBuilder<DocumentSnapshot>(
+          stream: clientsCollection.doc(userId).snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const CircularProgressIndicator();
-          }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
 
-          Map<String, dynamic> userData =
-              snapshot.data!.data() as Map<String, dynamic>;
+            Map<String, dynamic> userData =
+                snapshot.data!.data() as Map<String, dynamic>;
 
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Semantics(
-                      label: 'User avatar',
-                      child: CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.grey,
-                        backgroundImage: _getAvatarImage(userData),
-                        child: _getAvatarImage(userData) == null
-                            ? const Icon(Icons.person, size: 50)
-                            : null,
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                    Semantics(
-                      label: 'Chat with user',
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  ChatScreen(receiverId: userId),
-                            ),
-                          );
-                        },
-                        child: const Icon(
-                          Icons.chat_rounded,
-                          size: 32,
-                          color: Colors.blue,
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Semantics(
+                        label: 'User avatar',
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundColor: Colors.grey,
+                          backgroundImage: _getAvatarImage(userData),
+                          child: _getAvatarImage(userData) == null
+                              ? const Icon(Icons.person, size: 50)
+                              : null,
                         ),
                       ),
+                      const SizedBox(width: 20),
+                      Semantics(
+                        label: 'Chat with user',
+                        child: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    ChatScreen(receiverId: userId),
+                              ),
+                            );
+                          },
+                          child: const Icon(
+                            Icons.chat_rounded,
+                            size: 32,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  Semantics(
+                    label: 'Full Name',
+                    child: Text(
+                      'Full Name: ${userData['first_name']} ${userData['last_name']}',
+                      style: const TextStyle(fontSize: 18),
                     ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                Semantics(
-                  label: 'Full Name',
-                  child: Text(
-                    'Full Name: ${userData['first_name']} ${userData['last_name']}',
-                    style: const TextStyle(fontSize: 18),
                   ),
-                ),
-                const SizedBox(height: 10),
-                Semantics(
-                  label: 'username',
-                  child: Text(
-                    'Username: @${userData['username']}',
-                    style: const TextStyle(fontSize: 18),
+                  const SizedBox(height: 10),
+                  Semantics(
+                    label: 'username',
+                    child: Text(
+                      'Username: @${userData['username']}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          );
-        },
+                  const SizedBox(height: 20),
+                  Text(
+                    '${userData['first_name']}\'s Posts',
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  StreamBuilder<QuerySnapshot>(
+                    stream: postsCollection
+                        .where('userId', isEqualTo: userId)
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> postSnapshot) {
+                      if (postSnapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
+
+                      if (postSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return const CircularProgressIndicator();
+                      }
+
+                      return ListView(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        children: postSnapshot.data!.docs
+                            .map((doc) => PostItem(
+                                data: doc.data() as Map<String, dynamic>))
+                            .toList(),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
